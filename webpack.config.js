c864d6dccacee015c6763ff768fdbf078376afd7
MarkoMTP <marko.mtp8@gmail.com>
2024-02-27 21:24:54 +0100

    added lint babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Specify the path to your HTML template
+    }),
+  ],
+  devServer: {
+
+    static: path.join(__dirname, 'dist'),
+
+  },
+};
